66da0532cb2afde9fd890575f2a61333ca668518
Junior DAVON <junior-yao.davon@sogeti.com>
2023-02-24 15:51:11 +0100

    change app test file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tuto-githubactions'`, () => {
+  it(`should have as title 'CI:CD meeting'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tuto-githubactions');
+    expect(app.title).toEqual('CI:CD meeting');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tuto-githubactions app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('CI:CD meeting app is running!');
   });
 });
